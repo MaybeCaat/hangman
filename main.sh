808675d01f326c2d26bf5fbbef82d9c8b92feab6
MaybeCaat <vladislav.osipov.023@yandex.ru>
2023-04-29 11:07:27 +0300

    refactoring end game

--- main.sh
+++ main.sh
@@ -71,19 +71,17 @@ function game_over() {
     echo "Вы проиграли!"
     echo "Было загадано слово ${word[@]}"
     echo "Вы отгадали: ${correct_letters[@]}"
-    read -p "Введите q, чтобы выйти или любой другой символ, чтобы начать новую игру: " new
-    if [[ $new == "q" ]]
-    then
-        exit 0
-    else
-        echo
-        init_game
-    fi
+    choose_new_game
 }
 
 function win() {
     echo "Вы выиграли!"
     echo "Загаданное слово: ${word[@]}"
+    choose_new_game
+}
+
+function choose_new_game() {
+    echo
     read -p "Введите q, чтобы выйти или любой другой символ, чтобы начать новую игру: " new
     if [[ $new == "q" ]]
     then
